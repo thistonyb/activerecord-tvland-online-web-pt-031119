b80afb8622a21fca0adf66683e518e580b5018d8
Tony Baker <this.tonybaker@gmail.com>
2019-06-17 02:42:23 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_catchphrase_to_characters.rb
+++ db/migrate/006_add_catchphrase_to_characters.rb
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-class AddCatchphraseToCharacters < ActiveRecord::Migration[5.2]
-=======
 class AddCatchPhraseToCharacters < ActiveRecord::Migration[5.2]
->>>>>>> 19f90aac1cf6534c8ab200155065d11ecf9288d1
+
   def change
     add_column :characters, :catchphrase, :string
   end
