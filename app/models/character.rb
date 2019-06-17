14b5a89f9b755988bfcdbdf010593886acddb785
Tony Baker <this.tonybaker@gmail.com>
2019-06-17 02:42:42 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,11 +1,8 @@
 class Character < ActiveRecord::Base
   belongs_to :show
   belongs_to :actor
-<<<<<<< HEAD
 
   def say_that_thing_you_say
     "#{self.name} always says: #{self.catchphrase}"
   end
-=======
->>>>>>> 19f90aac1cf6534c8ab200155065d11ecf9288d1
 end
